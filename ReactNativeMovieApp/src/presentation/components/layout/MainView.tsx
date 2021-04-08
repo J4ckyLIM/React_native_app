a39010fa7f4a9f2eaf0060b66c3354486cfccf4e
J4ckyLIM <j4cky.lm@gmail.com>
2021-04-08 19:05:15 +0200

    feat: color

--- ReactNativeMovieApp/src/presentation/components/layout/MainView.tsx
+++ ReactNativeMovieApp/src/presentation/components/layout/MainView.tsx
@@ -1,4 +1,4 @@
-import { css, styled } from "../../theme";
+import { css, styled } from '../../theme';
 
 export const MainView = styled.View<{
   justifyContent?: 'center' | 'flex-start' | 'flex-end';
@@ -8,7 +8,7 @@ export const MainView = styled.View<{
   flex: 1;
 
   background-color: ${({ theme, backgroundColor }) =>
-    backgroundColor ?? theme.colors.backgroundColor};
+    backgroundColor ?? theme.colors.inAppDarkScreenBackgroundColor};
   color: ${({ theme }) => theme.colors.defaultTextColor};
   flex-direction: column;
 
