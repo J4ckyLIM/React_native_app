fca6372a6c3b4afda96db13f956ce01a919f6feb
J4ckyLIM <j4cky.lm@gmail.com>
2021-04-08 19:04:15 +0200

    feat: add query to search with text

--- ReactNativeMovieApp/src/presentation/screens/hooks/getMovieList.ts
+++ ReactNativeMovieApp/src/presentation/screens/hooks/getMovieList.ts
@@ -4,13 +4,42 @@ import { Movie } from '../../../domain/Movie';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 const API_TOKEN = 'b4d4c267aea648659aca8853a1f95666';
+const API_IMAGE_URL = 'https://image.tmdb.org/t/p/w1280';
 
-// export const getMovieList = ({ text }: { text?: string}) =>
-//   useMemo(async () => {
-//     const movieResponse = await axios.get(`${API_BASE_URL}/search/movie?api_key=${API_TOKEN}&language=en${text ? `&query=${text}` : ''}`)
-//     console.log(movieResponse);
-//     return movieResponse;
-//   }, [text]);
+export const getMoviesByTitle = async ({
+  text,
+}: {
+  text: string;
+}): Promise<Movie[]> => {
+  try {
+    const movieResponse = await axios.get(
+      `${API_BASE_URL}/search/movie?api_key=${API_TOKEN}&language=en&query=${text}
+      }`,
+    );
+    const movies: Movie[] = movieResponse.data.results.map(movie => {
+      return {
+        title: movie.title,
+        popularity: movie.popularity,
+
+        releaseDate: movie.release_date,
+
+        description: movie.overview,
+
+        voteAverage: movie.vote_average,
+
+        voteCount: movie.count,
+
+        posterPath: API_IMAGE_URL + movie.poster_path,
+
+        id: movie.id,
+      };
+    });
+    return movies;
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error(e);
+  }
+};
 
 export const getTopRatedMovie = async (): Promise<Movie[]> => {
   try {
@@ -30,7 +59,9 @@ export const getTopRatedMovie = async (): Promise<Movie[]> => {
 
         voteCount: movie.count,
 
-        posterPath: movie.poster_path,
+        posterPath: API_IMAGE_URL + movie.poster_path,
+
+        id: movie.id,
       };
     });
     return movies;
