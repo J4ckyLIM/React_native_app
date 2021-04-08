080de2a013f8ea33981d59715071cf47e7bf3f00
J4ckyLIM <j4cky.lm@gmail.com>
2021-04-08 19:06:54 +0200

    feat: add search bar and restyle app

--- ReactNativeMovieApp/src/presentation/screens/Home.tsx
+++ ReactNativeMovieApp/src/presentation/screens/Home.tsx
@@ -1,46 +1,72 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { NavigationInjectedProps } from 'react-navigation';
-import axios from 'axios';
 
 import { Routes } from '../../routes';
-import { MainButton } from '../components/button/Button';
 import { Block } from '../components/layout/Block';
 import { ScreenContainer } from '../components/layout/ScreenContainer';
 import { theme } from '../theme';
 import { Movie } from '../../domain/Movie';
 import { MovieItemList } from '../components/card/MovieItemList';
+import { SearchBar } from '../components/input/SearchBar';
 
-import { getTopRatedMovie } from './hooks/getMovieList';
+import { getMoviesByTitle, getTopRatedMovie } from './hooks/getMovieList';
 
 export const HomeScreen = ({ navigation }: NavigationInjectedProps) => {
-  const navigateToDetails = useCallback(() => {
-    navigation.navigate({ routeName: Routes.Details });
-  }, [navigation]);
-
   const [movies, setMovies] = useState<Movie[]>([]);
+
+  const showMovieDetail = useCallback(
+    (movieToDisplay: Movie) => {
+      navigation.navigate(Routes.Details, { movie: movieToDisplay });
+    },
+    [navigation],
+  );
+
+  const searchMovieByTitle = useCallback(
+    async (value: string) => {
+      let movieListBis: Movie[];
+      if (value !== '') {
+        movieListBis = await getMoviesByTitle({ text: value });
+      } else {
+        movieListBis = await getTopRatedMovie();
+      }
+      setMovies(movieListBis);
+    },
+    [setMovies],
+  );
 
   useEffect(() => {
     (async () => {
       const movieList = await getTopRatedMovie();
       setMovies(movieList);
     })();
-  }, []);
+  }, [setMovies]);
 
   return (
     <ScreenContainer
       mainViewProps={{
         style: {
-          backgroundColor: theme.colors.inAppScreenBackroundColor,
+          backgroundColor: theme.colors.inAppDarkScreenBackgroundColor,
           padding: theme.spacing.m,
         },
       }}
       headerTitle={'Home'}
-      showHeaderTitle={true}>
-      <Block>
-        <MainButton label={'Go to details'} onPress={navigateToDetails} />
+      showHeaderTitle={true}
+      headerTitleColor={'#FFFFFF'}>
+      <SearchBar
+        onChangeText={value => {
+          searchMovieByTitle(value);
+        }}
+      />
+      <Block marginTop={10}>
         {movies.length > 0 &&
           movies.map(movie => (
-            <MovieItemList movie={movie} key={movie.title} />
+            <MovieItemList
+              movie={movie}
+              key={movie.id}
+              onPress={mv => {
+                showMovieDetail(mv);
+              }}
+            />
           ))}
       </Block>
     </ScreenContainer>
