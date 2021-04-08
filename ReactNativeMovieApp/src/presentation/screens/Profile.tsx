e4116a410c397749c44ffcab9d6f762c92eb671a
J4ckyLIM <j4cky.lm@gmail.com>
2021-04-09 00:02:35 +0200

    feat: profil screen, not implemented

---
+++ ReactNativeMovieApp/src/presentation/screens/Profile.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { NavigationInjectedProps } from 'react-navigation';
+
+import { Block } from '../components/layout/Block';
+
+export const ProfileScreen = ({ navigation }: NavigationInjectedProps) => {
+  return <Block />;
+};
