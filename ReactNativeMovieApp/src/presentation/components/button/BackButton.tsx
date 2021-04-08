a67be733415ff5e50f115b9dadd114c8955ef2a6
J4ckyLIM <j4cky.lm@gmail.com>
2021-04-08 11:52:34 +0200

    feat: add back button

---
+++ ReactNativeMovieApp/src/presentation/components/button/BackButton.tsx
@@ -0,0 +1,64 @@
+import * as React from "react";
+import { styled } from "../../theme";
+import ArrowLeftWhite from '../../resources/svg_icons/arrowLeftWhite.svg';
+import ArrowLeftBlack from '../../resources/svg_icons/arrowLeftBlack.svg';
+
+export enum ArrowDirection {
+  left = 'left',
+  down = 'down',
+}
+
+export enum ArrowColor {
+  white = 'white',
+  black = 'black',
+}
+
+const Container = styled.SafeAreaView<{ top: number; left: number }>`
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  top: ${({ top }) => top}px;
+  left: ${({ left }) => left}px;
+`;
+
+const Touchable = styled.TouchableOpacity<{
+  backgroundColor: string;
+}>`
+  width: 40px;
+  height: 40px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ backgroundColor }) => backgroundColor};
+`;
+
+export const BackButton = ({
+  onPress,
+  top = 0,
+  left = 0,
+  arrowOrientation = ArrowDirection.left,
+  color,
+  backgroundColor = 'transparent',
+  rounded = false,
+}: {
+  onPress: () => void;
+  top?: number;
+  left?: number;
+  arrowOrientation?: ArrowDirection;
+  color?: ArrowColor;
+  backgroundColor?: string;
+  rounded?: boolean;
+}) => (
+  <Container top={top} left={left}>
+    <Touchable
+      onPress={onPress}
+      backgroundColor={backgroundColor}
+    >
+      {arrowOrientation === ArrowDirection.left &&
+        color === ArrowColor.black && <ArrowLeftBlack />}
+
+      {arrowOrientation === ArrowDirection.left &&
+        color === ArrowColor.white && <ArrowLeftWhite />}
+    </Touchable>
+  </Container>
+);
