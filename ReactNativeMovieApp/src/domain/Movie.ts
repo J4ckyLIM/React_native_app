3b9ebe3b5aed46f1aa19257fa8fc29634b0b6061
J4ckyLIM <j4cky.lm@gmail.com>
2021-04-09 09:26:19 +0200

    feat: add details to movie property

--- ReactNativeMovieApp/src/domain/Movie.ts
+++ ReactNativeMovieApp/src/domain/Movie.ts
@@ -3,20 +3,36 @@ export interface Movie {
    * Title of the movie
    */
   title: string;
-
+  /**
+   * Popularity of the movie
+   */
   popularity: number;
-
+  /**
+   * Release date of the movie
+   */
   releaseDate: string;
-
+  /**
+   * Description of the movie
+   */
   description: string;
-
+  /**
+   * Rate of the movie (O to 10)
+   */
   voteAverage: number;
-
+  /**
+   * Number of votes
+   */
   voteCount: number;
-
+  /**
+   * Url of the poster
+   */
   posterPath: string;
-
+  /**
+   * Url of the back poster
+   */
   backPosterPath: string;
-
+  /**
+   * Id of the movie
+   */
   id: number;
 }
